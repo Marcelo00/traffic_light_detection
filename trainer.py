1a23c1d205b6efb891dab5ee698d7510bb8cd8f9
Marcel Hiltscher <marcelhiltscher@outlook.de>
2020-02-18 21:17:17 +0100

    use logger for logging

--- trainer.py
+++ trainer.py
@@ -4,12 +4,12 @@ import torch
 import torchvision
 import gc
 import os
+import logging
 from torch.utils import data
 from torch.utils.data import DataLoader
 from datetime import datetime
 from data.dataset import BoschDataset
 from utils.utils import collate_fn
-from utils.utils import test_collate_fn
 from typing import Tuple
 
 
@@ -26,6 +26,7 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('--num_workers', type=int, default=0, help='Number of workers for the data loader')
     parser.add_argument('--batch_size', type=int, default=5, help='Batch Size for training')
     parser.add_argument('--start_eval', type=int, default=5, help='Epochs after which a eval run is started.')
+    parser.add_argument('--epochs', type=int, default=100, help='Epochs for training.')
     args = parser.parse_args()
     return args
 
@@ -48,33 +49,30 @@ def create_data_loader(args: argparse.Namespace) -> Tuple[DataLoader, DataLoader
                               batch_size=args.batch_size,
                               shuffle=True,
                               num_workers=args.num_workers,
-                              collate_fn=test_collate_fn)
+                              collate_fn=collate_fn)
     return train_dl, test_dl
 
 
-def start_evaluation(test_data_loader, model, device, output_path):
-    print("Start evaluation")
-    print('-' * 10)
+def start_evaluation(test_data_loader, model, device, output_path, epoch, logger):
+    logger.info(f'Start evaluation after {epoch} epochs')
     model.eval()
-    for idx, result in enumerate(train_data_loader):
+    for idx, result in enumerate(test_data_loader):
         images = list(image.to(device) for image in result[0])
         targets = result[1]
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         with torch.set_grad_enabled(False):
             outputs = model(images)
-            outputs = [{k: v.to(device) for k, v in t.items()} for t in outputs]
-            for element in outputs:
-                print(f'Scores {element["scores"]}')
+            for output_idx, element in enumerate(outputs):
+                predicted_labels = element['labels']
+                true_labels = targets[output_idx]['labels']
+                logger.info(f'Scores {element["scores"]} \n' 
+                            f'Labels predicted: {predicted_labels} Groundtruth labels: {true_labels}')
     save_model(path=output_path, model=model, epochs=epoch)
 
 
-def train_one_epoch(train_data_loader, model, device, optimizer, epoch):
+def train_one_epoch(train_data_loader, model, device, logger, optimizer):
     start = time.time()
-    avg_losses = []
+    loss_per_iteration = []
     for idx, result in enumerate(train_data_loader):
-        if idx > 5:
-            break
-        loss_per_iteration = []
         images = list(image.to(device) for image in result[0])
         targets = result[1]
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
@@ -82,17 +80,34 @@ def train_one_epoch(train_data_loader, model, device, optimizer, epoch):
         with torch.set_grad_enabled(True):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
-            print(loss_dict)
             losses.backward()
             optimizer.step()
             loss_per_iteration.append(losses)
-            avg_losses.append(torch.mean(torch.stack(loss_per_iteration)))
+            loss_doc_str = "".join("{}_{} ".format(key, val) for key, val in loss_dict.items())
         gc.collect()
         if idx % 10 == 0:
-            print(f'Iteration: [{idx}/{len(train_data_loader)}]\t'
-                  f'Loss {losses}')
+            logger.info(f'Iteration: [{idx}/{len(train_data_loader)}]\t'
+                        f'Loss: {losses} \t'
+                        f'Loss_dict: {loss_doc_str}')
     epoch_time = time.time() - start
-    return avg_losses, epoch_time
+    return loss_per_iteration, loss_dicts, epoch_time
+
+
+def creeate_logger(output_path):
+    logger = logging.getLogger(__name__)
+    logger.propagate = False
+    format_string = '%(asctime)s: %(message)s'
+    logger_filer_path = os.path.join(output_path, 'logger.txt')
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler(logger_filer_path, mode='w+')
+    c_handler.setLevel(logging.INFO)
+    f_handler.setLevel(logging.INFO)
+    handler_format = logging.Formatter(format_string, datefmt='%H:%M:%S')
+    c_handler.setFormatter(handler_format)
+    f_handler.setFormatter(handler_format)
+    logger.addHandler(f_handler)
+    logger.addHandler(c_handler)
+    return logger
 
 
 if __name__ == "__main__":
@@ -100,8 +115,8 @@ if __name__ == "__main__":
     current_time = datetime.now().strftime("%d_%b_%H:%M:%S")
     output_path = os.path.join(args.output_path, current_time)
     os.makedirs(output_path, exist_ok=True)
+    logger = creeate_logger(output_path)
     num_classes = 5
-    num_epochs = 10
     train_data_loader, test_data_loader = create_data_loader(args)
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, num_classes=num_classes)
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -109,15 +124,15 @@ if __name__ == "__main__":
     model.to(device)
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    for epoch in range(1, num_epochs + 1):
-        print(f'Epoch {epoch}/{num_epochs}')
-        print('-' * 10)
+    for epoch in range(1, args.epochs + 1):
+        logger.info(f'Epoch {epoch}/{args.epochs}')
         try:
-            avg_losses, epoch_time = train_one_epoch(train_data_loader, model, device, optimizer=optimizer, epoch=epoch)
-            print('-' * 10)
-            print(f'Epoch Avg Loss {avg_losses} '
-                  f'Epoch Runtime {epoch_time}')
+            loss_per_iteration, loss_dicts, epoch_time = train_one_epoch(train_data_loader, model, device, logger,
+                                                                         optimizer=optimizer)
+            avg_losses = torch.mean(torch.stack(loss_per_iteration))
+            logger.info(f'Epoch {epoch} avg Loss {avg_losses} with a runtime of {epoch_time}')
+            save_model(path=output_path, model=model, epochs=epoch)
         except:
             save_model(path=output_path, model=model, epochs=epoch)
         if epoch % args.start_eval == 0:
-            start_evaluation(test_data_loader, model, device, output_path)
+            start_evaluation(test_data_loader, model, device, output_path, epoch, logger)
