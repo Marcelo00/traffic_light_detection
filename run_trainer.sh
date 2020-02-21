5a16f6f55c8c7136bc480b18cfc66cda694f14f5
Marcel Hiltscher <marcelhiltscher@outlook.de>
2020-02-21 13:56:38 +0100

    add new args in batch script

--- run_trainer.sh
+++ run_trainer.sh
@@ -2,18 +2,20 @@
 ltrain='/content/drive/My Drive/bosch_dataset/train.yaml'
 ltest='/content/drive/My Drive/bosch_dataset/test.yaml'
 use_riib=True
-batch_size=5
-data_path='/content/drive/My Drive/bosch_dataset/data_reduced'
-start_eval=1
+batch_size_train=5
+batch_size_test=5
+data_path='/content/drive/My Drive/bosch_dataset/balanced_data'
+start_eval=10
 output_path='/content/drive/My Drive/models'
-device='cpu'
+device='cuda'
 num_workers=1
-epochs=100
+epochs=300
 
 python trainer.py -ltrain "$ltrain" \
 -ltest "$ltest" \
 --num_workers $num_workers \
---batch_size $batch_size \
+--batch_size_train $batch_size_train \
+--batch_size_test $batch_size_test \
 --use_riib $use_riib \
 --data_path "$data_path" \
 --start_eval $start_eval \
